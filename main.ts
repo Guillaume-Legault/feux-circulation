d4a683b55d1f83eddb96273a6563f286d157978c
Guillaume-Legault <93203429+Guillaume-Legault@users.noreply.github.com>
2021-11-26 14:01:22 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    A = 1
+})
+let A = 0
+A = 0
+basic.forever(function () {
+    if (A == 1) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        A = 0
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P8, 1)
+    }
+})
